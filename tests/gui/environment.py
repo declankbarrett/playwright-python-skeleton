aeebd901675e81a6039bbd80e64cabce9c7a0de7
Declan Barrett <declan.barrett@kainos.com>
2024-07-29 11:32:24 +0100

    Adding screenshots to allure report failures

--- tests/gui/environment.py
+++ tests/gui/environment.py
@@ -1,4 +1,7 @@
 from playwright.sync_api import sync_playwright
+import os
+from allure_commons.types import AttachmentType
+import allure
 
 def before_all(context):
     context.playwright = sync_playwright().start()
@@ -11,6 +14,16 @@ def before_feature(context, feature):
 def after_feature(context, feature):
     context.page.close()
     context.browser.close()
+    
+def after_step(context, step):
+    if step.status == "failed":
+        screenshot_dir = "reports/allure-results/screenshots"
+        if not os.path.exists(screenshot_dir):
+            os.makedirs(screenshot_dir)
+        screenshot_path = os.path.join(screenshot_dir, f"{step.name}.png")
+        context.page.screenshot(path=screenshot_path)
+        # Attach screenshot to Allure report
+        allure.attach.file(screenshot_path, name="Screenshot", attachment_type=AttachmentType.PNG)
 
 def after_all(context):
     context.playwright.stop()
